aee4fd63e160a0e8f87e0e46e592a23c072c9990
root <root@server1.mike>
2018-06-05 13:28:48 -0400

    fixed config for br0

--- kvm.sh
+++ kvm.sh
@@ -5,13 +5,13 @@ systemctl start libvirtd
 ownif=$(route | grep '^default' | grep -o '[^ ]*$')
 ## need to implement check that brX already exists and add br(X+1)
 echo "BRIDGE=br0" >> /etc/sysconfig/network-scripts/ifcfg-$ownif
-echo '"DEVICE="br0" \
+echo 'DEVICE="br0"
 # I am getting ip from DHCP server # \
-BOOTPROTO="dhcp" \
-IPV6INIT="yes" \
-IPV6_AUTOCONF="yes" \
-ONBOOT="yes" \
-TYPE="Bridge" \
+BOOTPROTO="dhcp"
+IPV6INIT="yes"
+IPV6_AUTOCONF="yes"
+ONBOOT="yes"
+TYPE="Bridge"
 DELAY="0" ' >> /etc/sysconfig/network-scripts/ifcfg-br0
 systemctl restart NetworkManager 
 systemctl restart network 
