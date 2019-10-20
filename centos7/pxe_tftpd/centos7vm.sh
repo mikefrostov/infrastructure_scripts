2fc32fa69152d86b3ddf1153138ebffefabe833e
mikefrostov <m.e.morozov1@gmail.com>
2019-10-20 10:52:09 -0400

    vagrant kubernetes virtualbox cluster

--- centos7/pxe_tftpd/centos7vm.sh
+++ centos7/pxe_tftpd/centos7vm.sh
@@ -4,7 +4,7 @@ virt-install \
 --ram 2048 \
 --vcpus=1 \
 --os-variant=centos7.0 \
---network=bridge=br0,model=e1000 \
+--network=bridge=enp2s0,model=e1000 \
 --graphics vnc,listen=0.0.0.0 --noautoconsole \
---cdrom /storage/CentOS-7-x86_64-Minimal-1810.iso \
+--cdrom /storage/CentOS-7-x86_64-Minimal-1908.iso \
 --disk path=/storage/pxevm.qcow2,size=40,bus=ide,format=qcow2
