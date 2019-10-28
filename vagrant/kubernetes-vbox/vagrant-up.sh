1f942e3e0ef5e0fb6887136956481e4ae2d4d2f3
mikefrostov <m.e.morozov1@gmail.com>
2019-10-28 13:29:30 -0400

    moved vagrant-kubernetes-vbox to a vagrant-virtualbox-ansible-kubernetes repository

--- vagrant/kubernetes-vbox/vagrant-up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-#looks like vagrant is asynchronous, so all vms are upped at the same time
-# and slaves don't have a chance to connect to the master cuz its not configured yet
-# so we need to up master first
-
-vagrant up k8s-master
-vagrant up
-
