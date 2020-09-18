5f7ff35ea6691a2879c41981cf830e71d694b8af
mikefrostov <m.e.morozov1@gmail.com>
2020-09-18 13:13:50 -0400

    fix firewalld

--- cmds/firewalld.sh
+++ cmds/firewalld.sh
@@ -1,3 +1,4 @@
 add port
-sudo firewall-cmd zone=public add-port=5000/tcp
-firewall-cmd zone=public add-port=2888/tcp
+sudo firewall-cmd --zone=public --add-port=5000/tcp
+firewall-cmd --zone=public add-port=2888/tcp
+firewall-cmd --reload
