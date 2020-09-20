66a1f634981f63e4bd5234b0fb6ecff0c6845a52
mikefrostov <m.e.morozov1@gmail.com>
2020-09-20 00:12:13 -0400

    centos8 ansible

--- centos8/vagrantinstall.sh
+++ centos8/vagrantinstall.sh
@@ -7,3 +7,9 @@ dnf install https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.rp
 #rpm -Uvh vagrant_2.2.4_x86_64.rpm
 CONFIGURE_ARGS="with-libvirt-include=/usr/include/libvirt with-libvirt-lib=/usr/lib64" vagrant plugin install vagrant-libvirt
 echo 'export VAGRANT_HOME=/home/VMs/vagrant' >> /etc/profile
+#dnf install python3 -y
+#dnf install python3-pip -y
+subscription-manager repos --enable ansible-2.8-for-rhel-8-x86_64-rpms
+dnf -y install ansible
+#pip3 install ansible 
+ansible --version
