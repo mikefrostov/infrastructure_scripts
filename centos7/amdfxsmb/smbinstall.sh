da67f18fe56e1613fb9d76523501f1f131c04a94
mikefrostov <m.e.morozov1@gmail.com>
2020-09-24 16:32:20 -0400

    smb fixes

--- centos7/amdfxsmb/smbinstall.sh
+++ centos7/amdfxsmb/smbinstall.sh
@@ -16,7 +16,7 @@ lsblk -l
 mount /dev/sda1 /mnt/4tb/
 mount /dev/sde1 /mnt/2tb/
 mount /dev/sdd2 /mnt/4tb2/
-sytemctl status smb
+systemctl status smb
 systemctl status nmb
 chcon -t samba_share_t /mnt/2tb/
 chmod -R 0755 /mnt/2tb/
