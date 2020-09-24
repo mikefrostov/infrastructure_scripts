9cab4c6651ada7edb5a1c182053b753d346bba22
mikefrostov <m.e.morozov1@gmail.com>
2020-09-24 16:29:47 -0400

    smb install

--- centos7/amdfxsmb/smbinstall.sh
+++ centos7/amdfxsmb/smbinstall.sh
@@ -1,4 +1,9 @@
-yum install samba samba-client samba-common
+#!/bin/bash
+set -e
+
+#samba install and configure
+yum install samba samba-client samba-common -y
+yum install ntfs-3g -y
 firewall-cmd --permanent --zone=public --add-service=samba
 firewall-cmd --reload
 cp /etc/samba/smb.conf /etc/samba/smb.conf.orig
@@ -6,52 +11,34 @@ cat /dev/null > /etc/samba/smb.conf
 cp ./smb.conf /etc/samba/smb.conf
 mkdir /mnt/2tb
 mkdir /mnt/4tb
-lsblk -l
-yum install ntfs-3g -y
-mount /dev/sdb1 /mnt/2tb/
+mkdir /mnt/4tb2
 lsblk -l
 mount /dev/sda1 /mnt/4tb/
-/mnt/4tb/
-/mnt/2tb/
+mount /dev/sde1 /mnt/2tb/
+mount /dev/sdd2 /mnt/4tb2/
 sytemctl status smb
 systemctl status nmb
 chcon -t samba_share_t /mnt/2tb/
+chmod -R 0755 /mnt/2tb/
+chown -R nobody:nobody /mnt/2tb/
 chcon -t samba_share_t /mnt/4tb/
 chmod -R 0755 /mnt/4tb/
 chown -R nobody:nobody /mnt/4tb/
+chcon -t samba_share_t /mnt/4tb2/
+chmod -R 0755 /mnt/4tb2/
+chown -R nobody:nobody /mnt/4tb2/ 
 systemctl enable smb.service
 systemctl enable nmb.service
 systemctl restart smb.service
 systemctl restart nmb.service
-ls /mnt/4tb/
-mount /dev/sda1 /mnt/4tb/
-chcon -t samba_share_t /mnt/2tb/
-mount /dev/sdb1 /mount/2tb
-mount /dev/sdb1 /mnt/2tb
-chcon -t samba_share_t /mnt/2tb/
-chcon -t samba_share_t /mnt/2tb
-chcon -t samba_share_t /mnt/4tb
-chown -R nobody:nobody /mnt/2tb
-chmod -R 777 /mnt/2tb
-systemctl restart smb
-systemctl restart nmb
-chcon -t samba_share_t /mnt/2tb
 #  vi /etc/fstab
-chcon -t samba_share_t /mnt/2tb
-ls /mnt/2tb/
-ls /mnt/4tb/
-chcon -t samba_share_t /mnt/2tb
-chcon -R -t samba_share_t /mnt/2tb
-selinux
+# echo '/dev/sda1 /mnt/2tb default 0 0 0 >> /etc/fstab
+#chcon -R -t samba_share_t /mnt/2tb # -t??
+
 sestatus
-selinux permissive
-selinuxenabled
-sestatus
-getenforce
 setenforce 0
-getenforce
-getenforce
-vi /etc/selinux/config
+sed -i -e 's/enforcing/permissive/g' /etc/selinux/config
+reboot
 yum install vsftpd -y
 systemctl enable vsftpd
 firewall-cmd --zone=public --permanent --add-port=21/tcp
@@ -59,7 +46,7 @@ firewall-cmd --zone=public --permanent --add-service=ftp
 firewall-cmd –-reload
 firewall-cmd --reload
 sudo cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.default
-vi /etc/vsftpd/vsftpd.conf
+#vi /etc/vsftpd/vsftpd.conf
 systemctl restart vsftpd
 systemctl status vsftpd
 mkdir -p /var/ftp/pub
@@ -67,19 +54,10 @@ chown nobody:nogroup /var/ftp/pub
 chown nobody:nobody /var/ftp/pub
 echo "vsftpd test file" | sudo tee /var/ftp/pub/test.txt
 ls /var/ftp/pub/
-vi /etc/vsftpd.conf
-vi /etc/vsftpd/vsftpd.conf
+#vi /etc/vsftpd.conf
+#vi /etc/vsftpd/vsftpd.conf
 systemctl status vsftpd
 systemctl restart vsftpd
 systemctl status vsftpd
-mount --bind /mnt/2tb/share/biblioteka/mylib/computer\ science/devops/virtual.cloud/Microsoft/ /var/ftp/pub/
-ls /var/ftp/pub/
-umount /var/ftp/pub/
-systemctl status vsftpd
-systemctl stop vsftpd
-systemctl status vsftpd
-umount /var/ftp/pub/
-systemctl start vsftpd
-ls /mnt/2tb/
-ls /mnt/4tb/
-getenforce
+#mount --bind /mnt/2tb/share/biblioteka/mylib/computer\ science/devops/virtual.cloud/Microsoft/ /var/ftp/pub/
+#umount /var/ftp/pub/
