f10a8b39b55a00f9b233346e8e116b44fcec5d6a
root <root@centos7.home>
2021-07-17 07:41:56 -0400

    workaround

--- centos7/vagrantinstall.sh
+++ centos7/vagrantinstall.sh
@@ -3,7 +3,8 @@ set -e
 
 yum group install -y "Development Tools"
 yum -y install qemu-kvm libvirt virt-install bridge-utils libvirt-devel  libxslt-devel libxml2-devel libvirt-devel libguestfs-tools-c
-yum install -y https://releases.hashicorp.com/vagrant/2.2.4/vagrant_2.2.4_x86_64.rpm
+yum install -y https://releases.hashicorp.com/vagrant/2.2.17/vagrant_2.2.17_x86_64.rpm
 #rpm -Uvh vagrant_2.2.4_x86_64.rpm
+sudo ln -s /opt/vagrant/embedded/include/ruby-3.0.0/ruby/st.h /opt/vagrant/embedded/include/ruby-3.0.0/st.h
 vagrant plugin install vagrant-libvirt
 echo 'export VAGRANT_HOME=/home/VMs/vagrant' >> /etc/profile
